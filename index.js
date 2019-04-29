d07c1d82cc3e9642184fc79ca1cc8fe40205cfc4
francisco <francisco.miranda.92@gmail.com>
2019-04-28 20:28:46 -0500

    cleaned up code

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ function getRandomMeme(endPoint) {
 try{
 
 client.on('message', msg=> {
+
+  let msg1 = msg.content.toLowerCase();
+
   if (msg.isMentioned(client.user)) {
     const rand = Math.floor(Math.random() * Math.floor(4));
     if(rand === 0){msg.reply('yes', { tts: false });}
@@ -31,77 +34,57 @@ client.on('message', msg=> {
     if(rand === 2){msg.reply('no', { tts: false });}
     if(rand === 3){msg.reply('maybe', { tts: false });}
 }
+  else if(msg1.includes("thank")) {
+  msg.reply("You're welcome")
+}
+  else{
+    switch (msg1) {
+
+      case '/randommeme':
+        getRandomMeme('gimme').then( (result) => {
+          if(result){
+            const memelink = JSON.parse(result).url;
+            msg.reply(memelink)
+          }
+          else{
+            msg.reply("error, result is empty")
+          }
+        })
+        .catch((err) => {
+          msg.reply("error, failed to get result")
+        });
+        break; 
+        
+      case '/kirbymeme':
+        getRandomMeme('gimme/kirby').then( (result) => {
+          if(result){
+            const memelink = JSON.parse(result).url;
+            msg.reply(memelink)
+          }
+          else{
+            msg.reply("error, result is empty")
+          }
+        })
+        .catch((err) => {
+          msg.reply("error, failed to get result")
+        });
+        break; 
+
+      case 'f':
+        for(i=0; i < 10; i++){
+          msg.reply('f', { tts: false })
+          }
+      break; 
+
+      case '/sadkirby':
+      msg.reply('https://data.whicdn.com/images/316839211/large.jpg')
+      break; 
+    }
+  }
+
 });
 
-client.on('message', msg => {
-  if (msg.content === '/randomMeme') {
-    getRandomMeme('gimme').then( (result) => {
-      if(result){
-        const memelink = JSON.parse(result).url;
-        msg.reply(memelink)
-      }
-      else{
-        msg.reply("error, result is empty")
-      }
-    })
-    .catch((err) => {
-      msg.reply("error, failed to get result")
-  });
-  }
-})
 
-/* client.on('message', msg => {
-  let msg1 = msg.content.toLowerCase();
-  if (msg1.includes("keyboard") || msg1.includes === "keyboards") {
-    msg.("no one wants to see your keyboards")
-  }
-})
- */
-
-client.on('message', msg => {
-  let msg1 = msg.content.toLowerCase();
-  if (msg1.includes("thank")) {
-    msg.reply("You're welcome")
-  }
-})
-
-client.on('message', msg => {
-  if (msg.content === '/kirbyMeme') {
-    getRandomMeme('gimme/kirby').then( (result) => {
-      if(result){
-        const memelink = JSON.parse(result).url;
-        msg.reply(memelink)
-      }
-      else{
-        msg.reply("error, result is empty")
-      }
-    })
-    .catch((err) => {
-      msg.reply("error, failed to get result")
-  });
-  }
-})
-
-client.on('message', msg => {
-  if (msg.content === '/sadkirby') {
-    msg.reply('https://data.whicdn.com/images/316839211/large.jpg')
-  }
-})
-
-client.on('message', msg => {
-  if (msg.content === 'f') {
-    for(i=0; i < 10; i++){
-    msg.reply('f', { tts: false })
-    }
-  }
-})
-client.on('message', msg => {
-  if (msg.content === 'F') {
-    for(i=0; i < 10; i++){
-    msg.reply('f', { tts: false })
-    }
-  }
-})
 }
-catch(err){console.log(err)}
+catch(err){console.log(err)} 
 client.login(process.env.TOKEN)
